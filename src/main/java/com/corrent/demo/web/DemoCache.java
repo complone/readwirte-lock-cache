e49df41acba117a343071b1b046b7c07b4518a36
chengxingyuan <1817802738@qq.com>
2019-07-09 14:41:47 +0800

    添加读写锁，保证缓存数据库最终一致性

---
+++ src/main/java/com/corrent/demo/web/DemoCache.java
@@ -0,0 +1,57 @@
+package com.corrent.demo.web;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class DemoCache<K,V> {
+
+    final Map<K,V> m  = new HashMap<>();
+
+    final ReadWriteLock rwl = new ReentrantReadWriteLock();
+    //读锁
+    final Lock r = rwl.readLock();
+    //写锁
+    final Lock w = rwl.readLock();
+    //读缓存
+    V get(K key){
+        V v = null;
+        r.lock();
+        try{
+            v =  m.get(key);
+        }finally {
+            r.unlock();
+        }
+        //缓存中存在，返回
+        if (v!=null){
+            return v;
+        }
+
+        //缓存中不存在，查询数据库
+        w.lock();
+        try{
+            //再次验证，其他线程可能已经查询过数据库
+            v = m.get(key);
+            if (v == null){
+                //查询数据库
+                v = m.get(key);
+                m.put(key,v);
+            }
+        }finally {
+            w.unlock();
+        }
+        return v;
+    }
+
+    //写缓存
+    V put(K key,V value){
+        w.lock();
+        try{
+            return m.put(key,value);
+        }finally {
+            w.unlock();
+        }
+    }
+}
